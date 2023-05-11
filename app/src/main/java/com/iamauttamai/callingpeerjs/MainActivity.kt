1fe3a789134a24a573dcf81e49846944a515261a
DESKTOP-HETNTT6\Auttaphon <auttaphon@efinancethai.com>
2023-05-11 13:41:26 +0700

    no message

--- app/src/main/java/com/iamauttamai/callingpeerjs/MainActivity.kt
+++ app/src/main/java/com/iamauttamai/callingpeerjs/MainActivity.kt
@@ -2,10 +2,14 @@ package com.iamauttamai.callingpeerjs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iamauttamai.callingpeerjs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
